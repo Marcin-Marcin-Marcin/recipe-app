b55ef8d9454574cd10174ff34438fd593afa0b4d
MarcinBiesiada <mtbiesiada@gmail.com>
2026-01-05 16:06:45 +0100

    Secret Key

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-qrh-r%1#lu=!e+a%$whgda*e@0d$7g(4+=z1hkb%ow=!p_!5!t"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
